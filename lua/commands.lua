d2c75dd99e4b47a6b65ce8308ca8c4347789c751
Rihards Paskausks <pundurs@glhf.lv>
2024-04-23 10:39:33 +0300

    add InsertDateTime command

--- lua/commands.lua
+++ lua/commands.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_user_command("OpenInExplorer", "!explorer %:p:h", {})
 vim.api.nvim_create_user_command("CurFileDir", "NvimTreeFindFile", {})
 vim.api.nvim_create_user_command("CopyRelativeFilePath", 'let @* = expand("%")', {})
 vim.api.nvim_create_user_command("MyTodos", "Rg TODO\\(rp\\)", {})
+vim.api.nvim_create_user_command(
+    "InsertDateTime",
+    function()
+        local currentDateTime = os.date("%Y-%m-%d %H:%M:%S")
+        vim.api.nvim_put({currentDateTime}, "", true, true)
+    end,
+    {}
+)
