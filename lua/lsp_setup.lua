3d560aee169f93f231c762365b3b9656f825a102
Rihards Paskausks <pundurs@glhf.lv>
2024-09-10 13:36:37 +0300

    tsserver (deprecated) -> ts_ls

--- lua/lsp_setup.lua
+++ lua/lsp_setup.lua
@@ -59,7 +59,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { "tsserver", "rust_analyzer", "clangd", "csharp_ls", "gopls", "pylsp"}
+local servers = { "ts_ls", "rust_analyzer", "clangd", "csharp_ls", "gopls", "pylsp"}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
