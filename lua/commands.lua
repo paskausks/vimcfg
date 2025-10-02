f693540ec481b3273b1dede5f0dd57dd32aa9f25
Rihards Paskausks <pundurs@glhf.lv>
2025-10-03 02:03:07 +0300

    restore BufOnly command

--- lua/commands.lua
+++ lua/commands.lua
@@ -8,3 +8,13 @@ vim.api.nvim_create_user_command(
     end,
     {}
 )
+
+vim.api.nvim_create_user_command("BufOnly", function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+      vim.api.nvim_buf_delete(buf, { })
+    end
+  end
+  vim.cmd("edit")  -- refresh buffer tab line
+end, {})
